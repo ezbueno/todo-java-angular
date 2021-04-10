088ae5228f88f8dd0f8e68aa9bcd1a54581920af
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-04-09 22:39:43 -0300

    changed from LocalDateTime to Date

--- todo/src/main/java/com/ezandro/todo/services/DBService.java
+++ todo/src/main/java/com/ezandro/todo/services/DBService.java
@@ -19,17 +19,13 @@ public class DBService {
 	public void instanciarBaseDados() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot 2 e Angular 11",
-				sdf.parse("25/03/2022"), false);
+		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot 2 e Angular 11", sdf.parse("25/03/2022"), false);
 
-		Todo t2 = new Todo(null, "Ler", "Ler livro de desenvolvimento pessoal",
-				sdf.parse("22/03/2021"), true);
+		Todo t2 = new Todo(null, "Ler", "Ler livro de desenvolvimento pessoal", sdf.parse("22/03/2021"), true);
 
-		Todo t3 = new Todo(null, "Exercícios", "Praticar exercícios físicos",
-				sdf.parse("21/03/2021"), false);
+		Todo t3 = new Todo(null, "Exercícios", "Praticar exercícios físicos", sdf.parse("21/03/2021"), false);
 
-		Todo t4 = new Todo(null, "Meditar", "Meditar durante 30 minutos pela manhã",
-				sdf.parse("27/03/2021"), true);
+		Todo t4 = new Todo(null, "Meditar", "Meditar durante 30 minutos pela manhã", sdf.parse("27/03/2021"), true);
 
 		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
 	}
